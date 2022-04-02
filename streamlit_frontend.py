731a83caf5f7687ec2220e2104852eaf7d97dd8a
MestreD <davidmestre86@gmail.com>
2022-04-02 08:35:38 +0200

    networkmaps and streamlit setting up

---
+++ streamlit_frontend.py
@@ -0,0 +1,80 @@
+import streamlit as st
+# Pandas: This library will used to create and work with dataframes.
+import pandas as pd 
+# Matplotlib: Used to plot graphics. 
+import matplotlib.pyplot as plt
+# Seaborn: Used to better style and improve graphics. 
+import seaborn as sns
+# Numpy for mathematic calculations. 
+import numpy as np
+#Tweepy: Used to work with the Twitter API.
+import tweepy as tw
+#ConfigParser: Used to get credentials for the twitter API.
+import configparser as cp
+#NLTK its used to pre-proccess text and regex its used to filter that text.
+import nltk, re
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+stop_words = stopwords.words('english')
+custom_stopwords = ["Tesla", "tesla", "TSLA", "tsla", "Rivian", "rivian", "RIVN", "rivn"]
+normalizer = WordNetLemmatizer()
+#Library to count words
+from collections import Counter
+# library to build wordclouds
+from wordcloud import WordCloud
+# NLTK to analice sentiment. 
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+sia = SentimentIntensityAnalyzer()
+#Another Library to perform sentiment analisis.
+from textblob import Word, TextBlob
+# NLTK library to build ngrams.
+from nltk.util import ngrams
+# NetworkX is a Python package for the creation, manipulation, and study of the structure, dynamics, and functions of complex networks.
+import networkx as nx
+from pyvis.network import Network
+# Built on top of plotly.js, plotly.py is a high-level, declarative charting library.
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+# Importing and authenticating API credentials from the config file. 
+config =  cp.ConfigParser()
+config.read("config.ini")
+
+api_key = config["twitter"]["api_key"]
+api_key_secret = config["twitter"]["api_key_secret"]
+access_token = config["twitter"]["access_token"]
+access_token_secret = config["twitter"]["access_token_secret"]
+
+# Authentication
+
+auth = tw.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tw.API(auth)
+
+#Functions to work with in this project.
+
+# Function to perform data extraction from twitter.
+
+
+
+# Streamlit web.
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+with header:
+    st.title("NLP analysis on two companies from twitter posts data.")
+    st.markdown("<h8 style='text-align: justify;'>This project is an approach to apply Natural Language Processing on twitter posts using tweepy API, to gain insight of the topics that interest the most people, public opinion and sentiment analysis about two different companies in the Electric Vehicles industrial sector.</h8>", unsafe_allow_html=True)
+
+with dataset:
+    st.header("Data Acquisition")  
+    st.markdown("<h8 style='text-align: justify;'>Tweepy is an open-source python package to access the Twitter API. Using this package, we can retrieve tweets of users, retweets etc. In our project, we will use this package to get live tweets based on two given search string and limiting the data extraction specifying the number of tweets desired.</h8>", unsafe_allow_html=True)
+    
