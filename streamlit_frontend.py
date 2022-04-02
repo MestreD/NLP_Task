bef6510df38df25b583660a050731bfd4b65d43a
MestreD <davidmestre86@gmail.com>
2022-04-02 09:21:55 +0200

    streamlit changes

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -41,6 +41,7 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
+
 # Importing and authenticating API credentials from the config file. 
 config =  cp.ConfigParser()
 config.read("config.ini")
@@ -57,10 +58,46 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tw.API(auth)
 
-#Functions to work with in this project.
+# Functions to work with in this project:
+
 
 # Function to perform data extraction from twitter.
+def scrape(words, numtweet):
+# We are using .Cursor() to search
+# through twitter for the required tweets.
+# The number of tweets can be
+# restricted using .items(number of tweets)
+    tweets = tw.Cursor(api.search_tweets,
+                               words, 
+                               lang="en",
+                               tweet_mode='extended').items(numtweet)
 
+
+# .Cursor() returns an iterable object. Each item in
+# the iterator has various attributes
+# that you can access to
+# get information about each tweet
+    list_tweets = [tweet for tweet in tweets]
+ 
+# we will iterate over each tweet in the
+# list for extracting information about each tweet
+    columns=['tweet_date','tweets']
+    data = []
+    for tweet in list_tweets:
+        tweet_date = tweet.created_at
+# Retweets can be distinguished by
+# a retweeted_status attribute,
+# in case it is an invalid reference,
+# except block will be executed
+        try:
+            tweets = tweet.retweeted_status.full_text
+        except AttributeError:
+            tweets = tweet.full_text
+            data.append([tweet_date, tweets])
+# Creating DataFrame using pandas
+    df = pd.DataFrame(data, columns=columns)
+    print(words + " Data.")
+    return df 
 
 
 # Streamlit web.
@@ -77,4 +114,10 @@ with header:
 with dataset:
     st.header("Data Acquisition")  
     st.markdown("<h8 style='text-align: justify;'>Tweepy is an open-source python package to access the Twitter API. Using this package, we can retrieve tweets of users, retweets etc. In our project, we will use this package to get live tweets based on two given search string and limiting the data extraction specifying the number of tweets desired.</h8>", unsafe_allow_html=True)
-    
+    tesla = scrape("TSLA", 100)
+    rivian = scrape("RIVN", 100)
+    st.markdown("<h3 style='text-align: centered; color=red'> Raw Data </h3>", unsafe_allow_html=True)
+    st.markdown("<h11 style='text-align: centered; color=red'> Tesla </h11>", unsafe_allow_html=True)
+    st.write(tesla.head())
+    st.markdown("<h11 style='text-align: centered; color=red'> Rivian </h11>", unsafe_allow_html=True)
+    st.write(rivian.head())
